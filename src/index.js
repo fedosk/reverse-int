963a2a7184676b4f92c6d4eafbb36ea27ee0cbc5
Федосевич Эдуард <fedosevich@ecm.local>
2020-09-14 15:06:54 +0300

    feat: add solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let array = n.toString().split('');
+    if(array[0] === '-') {
+        array.reverse().pop();
+        return +array.join('');
+    } else {
+        return +array.reverse().join('');
+  }
 }
+
